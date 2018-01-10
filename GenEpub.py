b9774313dfe12d38435526ec59297328d040f7ba
Hal Motley <inferno986returns@gmail.com>
2018-01-10 18:41:45 +0000

    Fixed seek error although still won't zip.
    The fine folks at StackOverflow have helped me fix the seek error, but epubcheck and all readers attempted (both Digital Editions and GPlay Books) dislike my .epub files.
    
    Also made improved the Pages section of the JSON file for metadata.

--- GenEpub.py
+++ GenEpub.py
@@ -11,7 +11,7 @@ with open('metadata.json') as json_file:
         data = json.load(json_file)
 
 #The ePub standard requires deflated compression and a compression order.
-zf = zipfile.ZipFile(data["fileName"] + '.epub', 'w', zipfile.ZIP_STORED)
+zf = zipfile.ZipFile(data["fileName"] + '.epub', mode='w', compression=zipfile.ZIP_STORED)
 
 zf.write(data["fileName"] + '/mimetype')
 
@@ -25,14 +25,14 @@ for dirname, subdirs, files in os.walk(data["fileName"] + '/EBOOK'):
     for filename in files:
         zf.write(os.path.join(dirname, filename))
 
+zf.close()
+
 #zipfile has a built-in validator for debugging
-if zipfile.is_zipfile(zf) is True:
-    print("ZIP file is valid.")
+with open(data["fileName"] + '.epub','r') as f:
+    if zipfile.is_zipfile(f) is True:
+        print("ZIP file is valid.")
 
 #Extra debugging information
 #print(getinfo.compress_type(zf))
 #print(getinfo.compress_size(zf))
 #print(getinfo.file_size(zf))
-
-zf.close(zf)
-
