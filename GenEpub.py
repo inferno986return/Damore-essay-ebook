0f3ffb91816b5b3214149047c40bda77a757d93d
Hal Motley <inferno986returns@gmail.com>
2018-01-18 17:57:58 +0000

    Trying to write to root
    Trying to write the folders to the root of the zip so it can pass epubcheck. Not working out so far.

--- GenEpub.py
+++ GenEpub.py
@@ -11,19 +11,22 @@ with open('metadata.json') as json_file:
     data = json.load(json_file)
 
 #The ePub standard requires deflated compression and a compression order.
-zf = zipfile.ZipFile(data["fileName"] + '.epub', mode='w', compression=zipfile.ZIP_DEFLATED)
+zf = zipfile.ZipFile(data["fileName"] + '.epub', mode='w', compression=zipfile.ZIP_STORED)
 
-zf.write(data["fileName"] + '/mimetype')
+zf.write(data["fileName"] + '/mimetype', 'mimetype')
 
 for dirname, subdirs, files in os.walk(data["fileName"] + '/META-INF'):
-    zf.write(dirname)
     for filename in files:
         zf.write(os.path.join(dirname, filename))
+        print('dirname:' + dirname)
+        print('filename:' + filename)
 
 for dirname, subdirs, files in os.walk(data["fileName"] + '/OEBPS'):
     zf.write(dirname)
     for filename in files:
         zf.write(os.path.join(dirname, filename))
+        print('dirname:' + dirname)
+        print('filename:' + filename)
 
 zf.close()
 
