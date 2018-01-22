3c52815079b084e666a7ed6c4d6a69a7aeaf0482
Hal Motley <inferno986returns@gmail.com>
2018-01-22 21:20:39 +0000

    Made some changes to GenEpub.py and the Foreword
    I have mostly altered the Foreword to include some information about the lawsuit. I best also modify the content.opf with the new images and add id to the subheadings at a later date.

--- GenEpub.py
+++ GenEpub.py
@@ -16,6 +16,7 @@ zf = zipfile.ZipFile(data["fileName"] + '.epub', mode='w', compression=zipfile.Z
 zf.write(data["fileName"] + '/mimetype', 'mimetype')
 
 for dirname, subdirs, files in os.walk(data["fileName"] + '/META-INF'):
+    zf.write(dirname)
     for filename in files:
         zf.write(os.path.join(dirname, filename))
         print('dirname:' + dirname)
@@ -31,7 +32,7 @@ for dirname, subdirs, files in os.walk(data["fileName"] + '/OEBPS'):
 zf.close()
 
 #zipfile has a built-in validator for debugging
-with open(data["fileName"] + '.epub','r') as f:
+with open(data["fileName"] + '.epub', 'r') as f:
     if zipfile.is_zipfile(f) is True:
         print("ZIP file is valid.")
 
