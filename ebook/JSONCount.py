b4364b3ca46dd8419196495bbb2740cf578cc588
Hal Motley <inferno986returns@gmail.com>
2018-02-06 21:57:53 +0000

    Cleanup, got rid of JSONCount.py
    JSONCount.py was a temporary file to experiment with counting JSON elements. That code was implemented into GenMetadata.py.

--- ebook/JSONCount.py
+++
@@ -1,7 +0,0 @@
-import json
-from collections import OrderedDict
-
-with open("metadata.json") as json_file:
-    data = json.load((json_file), object_pairs_hook=OrderedDict) #For some reason the order is randomised, this preserves the order.
-
-print len(data["pages"])
